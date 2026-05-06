d2e80d5953ebc654fff2d4ee8a828f58a9e8f56e
PCnet <PCnet@DESKTOP-P57QAV8>
2026-05-06 09:37:15 -0700

    pariking manger

--- src/main/java/com/handmadeapp/handmademarketplaceapp/DB/DBConnection.java
+++ src/main/java/com/handmadeapp/handmademarketplaceapp/DB/DBConnection.java
@@ -9,7 +9,7 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String URL  = "jdbc:mysql://localhost:3306/handmadetest_db";
     private static final String USER = "root";
-    private static final String PASS = "";  // your MySQL password
+    private static final String PASS = "";  
 
     private static Connection connection;
 
--- src/main/java/com/handmadeapp/handmademarketplaceapp/DashboardController.java
+++ src/main/java/com/handmadeapp/handmademarketplaceapp/DashboardController.java
@@ -15,9 +15,6 @@ import javafx.fxml.Initializable;
  */
 public class DashboardController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
